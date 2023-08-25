7a9ba98b37ca2af1cdcd7d475fc6ba324a24abea
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-08-25 11:44:11 +0530

    Update MailConfig.java

--- Backend/src/main/java/com/mytimeaway/MailConfig.java
+++ Backend/src/main/java/com/mytimeaway/MailConfig.java
@@ -1,9 +1,12 @@
 package com.mytimeaway;
 
+import java.util.Properties;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class MailConfig {
@@ -22,6 +25,16 @@ public class MailConfig {
 
 	@Bean
 	public JavaMailSender javaMailSender() {
-		return null;
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost(host);
+		mailSender.setPort(port);
+		mailSender.setUsername(username);
+		mailSender.setPassword(password);
+
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+
+		return mailSender;
 	}
 }
